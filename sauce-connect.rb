533c79129c5709d9d12355f1468b62470bd39331
David Stone <david@stabbylambda.com>
2015-07-13 13:47:07 -0700

    Upgrade sauce connect to 4.3.9

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -3,8 +3,8 @@ require "formula"
 
 class SauceConnect < Formula
   homepage "https://docs.saucelabs.com/reference/sauce-connect/"
-  url "https://saucelabs.com/downloads/sc-4.3.6-osx.zip"
-  sha1 "0921965149b07ec90296aa2757d35c54e43cd197"
+  url "https://saucelabs.com/downloads/sc-4.3.9-osx.zip"
+  sha1 "b15857b60c8b6e1105a241c9faebd7896192c1e0"
 
   def install
     bin.install 'bin/sc'
