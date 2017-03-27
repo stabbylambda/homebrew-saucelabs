1a88b6dfbcbe6cebd7edce171fb3bbf4b8435ad4
Thorben Schröder <info@thorbenschroeder.de>
2017-03-27 17:04:25 -0400

    Updates SC version

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -3,8 +3,8 @@ require "formula"
 
 class SauceConnect < Formula
   homepage "https://docs.saucelabs.com/reference/sauce-connect/"
-  url "https://saucelabs.com/downloads/sc-4.3.11-osx.zip"
-  sha1 "5d0aa851d21f3d4a21f298b6a921761c6aa15217"
+  url "https://saucelabs.com/downloads/sc-4.4.5-osx.zip"
+  sha1 "49829d719e46a398504deda77dc3a13ac8b31dd9"
 
   def install
     bin.install 'bin/sc'
