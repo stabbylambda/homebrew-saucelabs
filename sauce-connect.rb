ab6b119c5f4851e7951861c7a12f7b75355393a0
mglolmos <mgl.olmos@gmail.com>
2017-09-05 16:54:14 +0200

    Updated homepage URL

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -2,7 +2,7 @@ require "formula"
 
 
 class SauceConnect < Formula
-  homepage "https://docs.saucelabs.com/reference/sauce-connect/"
+  homepage "https://wiki.saucelabs.com/display/DOCS/Sauce+Connect+Proxy"
   url "https://saucelabs.com/downloads/sc-4.4.9-osx.zip"
   sha256 "c88731ffe03c1f0c7ff98e0036a467ea5bce1ff757b27b31c85d6e6e219cdfc5"
 
